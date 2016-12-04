62c6efef21c5b4439f14780e08bfb849d4c2f7a5
woodyjl <woodyjeanlouis@gmail.com>
2016-12-04 16:29:52 -0500

    finished login

--- TheSauce2/app/src/main/java/com/carolinagold/thesauce/PermissionHandler.java
+++ TheSauce2/app/src/main/java/com/carolinagold/thesauce/PermissionHandler.java
@@ -1,11 +1,15 @@
 package com.carolinagold.thesauce;
 
+import android.annotation.TargetApi;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 /**
  * Created by woodyjean-louis on 12/4/16.
@@ -31,15 +35,35 @@ public class PermissionHandler extends Object implements ActivityCompat.OnReques
 
         if (callingActivity != null) {
             for(int i = 0; i < permissionsArray.length; i++) {
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+                    callBack.resultFromRequest(permissionsArray[i], PackageManager.PERMISSION_GRANTED);
+                    try {
+                        finalize();
+                    } catch (Throwable t) {
+                        Log.i(Logs.ERROR_IN_TRY, "something went wrong when calling finalize");
+                        t.printStackTrace();
+                    }
+                }
                 if (ContextCompat.checkSelfPermission(callingActivity, permissionsArray[i]) != PackageManager.PERMISSION_GRANTED) {
                     if (ActivityCompat.shouldShowRequestPermissionRationale(callingActivity, permissionsArray[i])) {
                         // Show an explanation on why you need an to use this feature of the phone
                         // Show using a dialog
-                        // showAlertDialog();
+                        Snackbar.make(callingActivity.getWindow().getDecorView().findViewById(android.R.id.content),
+                                R.string.permission_rationale,
+                                Snackbar.LENGTH_INDEFINITE)
+                                .setAction(android.R.string.ok, new View.OnClickListener() {
+                                    @Override
+                                    @TargetApi(Build.VERSION_CODES.M)
+                                    public void onClick(View v) {
+                                        ActivityCompat.requestPermissions(callingActivity,permissionsArray, PERMISSION_REQUEST);
+                                    }
+                                });
+
 
                     } else {
                         // The user did not recently deny the request, so you can ask for the first time
                         ActivityCompat.requestPermissions(callingActivity,permissionsArray, PERMISSION_REQUEST);
+                        break;
                     }
                 } else {
                     callBack.resultFromRequest(permissionsArray[i], PackageManager.PERMISSION_GRANTED);
